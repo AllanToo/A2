2cd0faee37b5a568f6bbbe0e552f8d391e975ee4
Paul Holck <113456542+PaulHolckSAIT@users.noreply.github.com>
2022-11-13 13:33:25 -0700

    Add files via upload
    Command line args are now accepted, the default, hardcoded test path was removed

--- A2Group1/src/driver/Driver.java
+++ A2Group1/src/driver/Driver.java
@@ -6,8 +6,14 @@ import java.io.IOException;
 	public class Driver {
 		public static void main(String[] args) throws IOException 
 		{ 	
-			String file = "res/sample2.xml";
-			Parser xml = new Parser(file);
+			if (args[0] == null) {
+				System.out.println("Please enter a file name.");
+			}
+			else {
+				String fileName = args[0];
+				String file = "res/" + fileName;
+				//String file = "res/sample2.xml";
+				Parser xml = new Parser(file);
+			}
 		}
-
 }
