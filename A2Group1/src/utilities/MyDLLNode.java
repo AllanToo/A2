502163ec80806e582b5fb21f2dada5c0404ce700
Allan To <113399719+AllanToo@users.noreply.github.com>
2022-11-12 19:00:43 -0700

    Update MyDLLNode.java

--- A2Group1/src/utilities/MyDLLNode.java
+++ A2Group1/src/utilities/MyDLLNode.java
@@ -1,20 +1,45 @@
 package utilities;
 
+import org.w3c.dom.Node;
+
 public class MyDLLNode<E> {
-      public E element;
-      public MyDLLNode<E> prev, next;
+       E element;
+      MyDLLNode previous;
+      MyDLLNode next;
+
       
       // constructor
       public MyDLLNode(E element)
       {
     	  this.element = element;
       }
-      public MyDLLNode ( E elem, MyDLLNode<E> prev, MyDLLNode<E> next)
+      public MyDLLNode ( E element, MyDLLNode<E> previous, MyDLLNode<E> next)
       {
-    	  this.element = elem;
-    	  this.prev = prev;
-    	  this.next = next;
+    	  this.element = element;
+    	  this.previous = previous;
+    	  this.next =  next;
+    	 
     	  
       }
+	public E getElement() {
+		return element;
+	}
+	public void setElement(E element) {
+		this.element = element;
+	}
+	public MyDLLNode<E> getPrevious() {
+		return previous;
+	}
+	public void setPrevious(MyDLLNode<E> previous) {
+		this.previous = previous;
+	}
+	public MyDLLNode<E> getNext() {
+		return next;
+	}
+	public void setNext(MyDLLNode next) {
+		this.next = next;
+	}
+      
+      
       
 }
