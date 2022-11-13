d0a59b1e66b7f492e60c08a377060d09d79505b3
Xyrille Tugade <113451914+XyrilleTugade@users.noreply.github.com>
2022-11-13 16:19:09 -0700

    Added Tests except for a few

--- A2Group1/src/test/MyDLLTests.java
+++ A2Group1/src/test/MyDLLTests.java
@@ -1,42 +1,74 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+
+import utilities.MyDLL;
 
 public class MyDLLTests {
       @Test
       void returnSizeFromTheList( )
       {
+    	  MyDLL<Integer> zeroDll = new MyDLL<Integer>();
+    	  MyDLL<Integer> numDll = new MyDLL<Integer>();
+    	  numDll.add(4);
+    	  numDll.add(7);
+    	  numDll.add(19);
+    	  numDll.add(12);
     	  
+    	  assertEquals(4, numDll.size());
+    	  assertEquals(0, zeroDll.size());
       }
       
       @Test
       void removeElementsFromList( )
       {
+    	  MyDLL<String> dll = new MyDLL<String>();
+    	  dll.add("Hello");
+    	  dll.add("Okay");
+    	  dll.add("Alright");
+    	  dll.add("Goodbye");
     	  
+    	  dll.remove("Goodbye");
+    	  assertFalse(dll.contains("Goodbye"));
       }
       
       @Test
       void insertElementAtSpecificIndex( )
       {
+    	  MyDLL<Integer> dll = new MyDLL<Integer>();
+    	  dll.add(2, 24);
+    	  int index = dll.get(2);
     	  
+    	  assertEquals(dll.contains(index), 24);
       }
       @Test
       void addElementToTheEndList( )
       {
-    	  
+    	  //MyDLL<Integer> dll = new MyDLL<Integer>();
       }
       
       @Test
       void addAllElementsToTheList( )
       {
-    	  
+    	  //MyDLL<Integer> dll = new MyDLL<Integer>();
       }
       
       
       @Test
       void getElementInSpecifiedIndex( )
       {
+    	  MyDLL<Integer> dll = new MyDLL<Integer>();
+    	  int index = dll.get(2);
     	  
+    	  dll.add(24);
+    	  dll.add(34);
+    	  dll.add(44);
+    	  dll.add(54);
+    	  
+    	  dll.get(2);
+    	  assertEquals(dll.contains(index), 44); 
       }
       
 }
