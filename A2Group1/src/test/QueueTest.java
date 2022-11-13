dff0bf248719a0de735b27c2318767b12c612d0f
Xyrille Tugade <113451914+XyrilleTugade@users.noreply.github.com>
2022-11-13 16:26:59 -0700

    First iteration of the Queue Test

---
+++ A2Group1/src/test/QueueTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.Test;
+
+import utilities.Queue;
+
+public class QueueTest {
+	@Test
+	void addObject() {
+		
+	}
+	
+	void removeObject() {
+		
+	}
+	
+	void peekObject() {
+		
+	}
+	
+	void removeAllObjects() {
+		
+	}
+	
+	void checkIfEmpty() {
+		
+	}
+	
+	void checkIfFull() {
+		
+	}
+	
+	void checkQueueSize() {
+		
+	}
+}
