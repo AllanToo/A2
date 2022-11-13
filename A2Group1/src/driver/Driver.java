11a3a5c7a0a21ae943dd53165b20f347664ace13
Paul Holck <113456542+PaulHolckSAIT@users.noreply.github.com>
2022-11-12 20:12:42 -0700

    Add files via upload
    Added driver package and class. Has hardcoded values that need to be changed on final product

---
+++ A2Group1/src/driver/Driver.java
@@ -0,0 +1,13 @@
+package driver;
+
+import utilities.*;
+import java.io.IOException;
+
+	public class Driver {
+		public static void main(String[] args) throws IOException 
+		{ 	
+			String file = "res/sample2.xml";
+			Parser xml = new Parser(file);
+		}
+
+}
