6d4bb6507c13ab9f91723be5f23fc3fd2bf98907
Justin Gil <113378840+Jjuicetin@users.noreply.github.com>
2022-11-14 16:46:34 -0700

    removed util package

--- A2Group1/src/test/MyArrayListTest.java
+++ A2Group1/src/test/MyArrayListTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-import java.util.Iterator;
+
 
 class MyArrayListTest<E> {
 	
