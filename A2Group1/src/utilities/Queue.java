3846913e3959fe00add7634cdf8da02d67c8886c
Allan To <113399719+AllanToo@users.noreply.github.com>
2022-11-14 01:22:26 -0700

    Update Queue.java

--- A2Group1/src/utilities/Queue.java
+++ A2Group1/src/utilities/Queue.java
@@ -1,69 +1,85 @@
 package utilities;
 
+import java.util.NoSuchElementException;
+
+import org.w3c.dom.Node;
 import org.w3c.dom.events.EventException;
 
-public class Queue <E> implements QueueADT{
+
+
+public class Queue <E> implements QueueADT, Iterator {
 	
 	// Declare variables
-	private  E frontElement;
-	private E lastElement;
-	private   Object[]  array;
-	private static int length;
-	private static int count;
+	private static Node front, rear;
+	private static int capacity;
+	private static int queueArray[];
+	private MyDLLNode<E> root;
+	private MyDLL<Object> queue = new MyDLL();
 	
 	
+	
+	Queue()
+	{
+		
+	}
 	//Constructor
 	Queue( int size)
 	{
-		frontElement = lastElement = null;
-		length = size;
-		array = new Object[length];
+		//front = rear = 0;
+		capacity = size;
+		queueArray = new int [capacity];
+		
 	}
 /* *
  * This method will add the element in the last position of the queue
  * @param toAdd the item to be added to the Queue.
  * @throws NullPointerException raised when a <code>null</code> object
- * 
+ * s
  */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void enqueue(Object toAdd) throws NullPointerException {
-		   if (isFull())
-		   {
-			   System.out.println("Queue is full ");
-			   
-		   }
-		   
-		//   lastElement = (lastElement +1) % length;
-	//	   array[lastElement] = toAdd;
-		   count++;
+	       
+	       capacity++;
+	   
+	       System.out.println(queue.add(size() -1,toAdd));
 		
 	}
 
 	@Override
-	public Object dequeue() throws EventException {
-		// TODO Auto-generated method stub
-		return null;
+	public Object dequeue() throws EmptyQueueException {
+		
+		if (isEmpty() == true)
+		{
+			throw new EmptyQueueException("Counter cannot be decremented.");
+			
+		}
+		capacity--;
+		return queue.remove(0);
 	}
 
 	@Override
 	public Object peek() throws EventException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return queue.get(0);
 	}
 
 	@Override
 	public void dequeueAll() {
-		// TODO Auto-generated method stub
+		front = null;
+		rear = null;
+		capacity = 0;
+		
 		
 	}
 
 	@Override
 	public boolean isEmpty() {
-	 if (size() == 0 )
+	 if (size() == 0)
 	 {
 		 return true;
 	 }
-		return false;
+	 return false;
 	}
 
 	@Override
@@ -74,14 +90,27 @@ public class Queue <E> implements QueueADT{
 
 	@Override
 	public boolean equals(QueueADT that) {
-		// TODO Auto-generated method stub
-		return false;
+		if (this == that)
+			return true;
+		if (that == null)
+			return false;
+		if (!(that instanceof Queue))
+			return false;
+		Queue other = (Queue) that;
+		if (root == null) {
+			if (other.root != null)
+				return false;
+		} else if (!root.equals(other.root))
+			return false;
+		if (capacity != other.capacity)
+			return false;
+		return true;
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return    queue.toArray();
 	}
 
 	@Override
@@ -92,15 +121,24 @@ public class Queue <E> implements QueueADT{
 
 	@Override
 	public boolean isFull() {
-		 
-		return (size() == length);
+		     
+		return false;
 	}
 
 	@Override
 	public int size() {
 		// last index + 1 
 		
-		return length; 
+		return capacity; 
+	}
+	@Override
+	public boolean hasNext() {
+		return queue.hasNext();
+		}
+	@Override
+	public Object next() throws NoSuchElementException {
+		
+		return queue.next();
 	}
 
 }
