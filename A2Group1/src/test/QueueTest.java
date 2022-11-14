fa0ad7c7eaec4e7545969d049de0993a71dcaa9b
Xyrille Tugade <113451914+XyrilleTugade@users.noreply.github.com>
2022-11-13 22:01:05 -0700

    Added some tests and imports

--- A2Group1/src/test/QueueTest.java
+++ A2Group1/src/test/QueueTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import utilities.Queue;
@@ -7,7 +10,14 @@ import utilities.Queue;
 public class QueueTest {
 	@Test
 	void addObject() {
+		Queue<Integer> que = new Queue<Integer>(0);
+		que.enqueue(23);
+		que.enqueue(42);
+		que.enqueue(56);
+		que.enqueue(78);
+		que.enqueue(95);
 		
+		assertTrue(que.size() > 0);
 	}
 	
 	void removeObject() {
@@ -23,14 +33,25 @@ public class QueueTest {
 	}
 	
 	void checkIfEmpty() {
+		Queue<Integer> que = new Queue<Integer>(0);
 		
+		assertTrue(que.isEmpty());
 	}
 	
 	void checkIfFull() {
+		Queue<Integer> que = new Queue<Integer>(0);
 		
+		assertTrue(que.isFull());
 	}
 	
 	void checkQueueSize() {
+		Queue<Integer> que = new Queue<Integer>(0);
+		que.enqueue(19);
+		que.enqueue(7);
+		que.enqueue(62);
+		que.enqueue(35);
+		que.enqueue(80);
 		
+		assertEquals(que.size(), 5);
 	}
 }
