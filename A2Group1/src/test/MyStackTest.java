7edbfc49352106b66e4ef56b84fd9f5f4a81cd76
Justin Gil <113378840+Jjuicetin@users.noreply.github.com>
2022-11-14 18:16:51 -0700

    Update MyStackTest.java

--- A2Group1/src/test/MyStackTest.java
+++ A2Group1/src/test/MyStackTest.java
@@ -1,8 +1,10 @@
 package test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
 
 import org.junit.Test;
 import utilities.MyStack;
